0e99cb469c5bc44a25fd1c380882e538c19c1038
IceRain0912 <brandon20010912@gmail.com>
2021-10-05 14:55:51 +0800

    2021.10.5 pm.2:51, hw1

---
+++ hw1/Jing-Tai-Xiang-Bo/script.js
@@ -0,0 +1,73 @@
+function copyPic1(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/1.jpg";
+}
+
+function copyPic2(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/2.jpg";
+}
+function copyPic3(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/3.jpg";
+}
+function copyPic4(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/4.jpg";
+}
+function copyPic5(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/5.jpg";
+}
+function copyPic6(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/6.jpg";
+}
+function copyPic7(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/7.jpg";
+}
+function copyPic8(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/8.jpg";
+}
+function copyPic9(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/9.jpg";
+}
+function copyPic10(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/10.jpg";
+}
+function copyPic11(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/11.jpg";
+}
+function copyPic12(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/12.jpg";
+}
+function copyPic13(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/13.jpg";
+}
+function copyPic14(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/14.jpg";
+}
+function copyPic15(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/15.jpg";
+}
+function copyPic16(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/16.jpg";
+}
+function copyPic17(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/17.jpg";
+}
+function copyPic18(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/18.jpg";
+}
+function copyPic19(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/19.jpg";
+}
+function copyPic20(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/20.jpg";
+}
+function copyPic21(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/21.jpg";
+}
+function copyPic22(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/22.jpg";
+}
+function copyPic23(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/23.jpg";
+}
+function copyPic24(){
+  document.getElementById("0").src = "https://430df76f-24a6-46cc-9167-2d989f8141dd.id.repl.co/images/24.jpg";
+}
